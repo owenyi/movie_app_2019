58b8ce78cd98dd255bfb239e29ff2e342124065a
LAPTOP-9D74L3IT\oweny <owenyi3207@gmail.com>
2020-09-22 17:48:59 +0900

    Updates

---
+++ precache-manifest.68e78c948f4d5c5f8f8cdb707c537f39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  },
+  {
+    "revision": "821af225dce5d515e9a5",
+    "url": "/movie_app_2019/static/js/main.495e10b3.chunk.js"
+  },
+  {
+    "revision": "d5cc5b87e6d6a7dac363",
+    "url": "/movie_app_2019/static/js/2.ff66ff15.chunk.js"
+  },
+  {
+    "revision": "821af225dce5d515e9a5",
+    "url": "/movie_app_2019/static/css/main.e3e1a038.chunk.css"
+  },
+  {
+    "revision": "0ec054adacfc7fb62b5640ec242509a8",
+    "url": "/movie_app_2019/index.html"
+  }
+];
